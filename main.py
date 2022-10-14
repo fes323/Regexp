69c42ce11b4860705e91b12a71e8d51717725558
Maksim legeyda <Maksim.legeyda@yandex.ru>
2022-10-14 23:36:22 +0300

    сделаю в субботу. Вообще нет времени

---
+++ main.py
@@ -0,0 +1,13 @@
+from pprint import pprint
+import csv
+import re
+
+with open("phonebook_raw.csv", encoding="utf-8") as f:
+  rows = csv.reader(f, delimiter=",")
+  contacts_list = list(rows)
+pprint(contacts_list)
+
+with open("phonebook.csv", "w", encoding="utf-8") as f:
+  datawriter = csv.writer(f, delimiter=',')
+  # Вместо contacts_list подставьте свой список
+  datawriter.writerows(contacts_list)
